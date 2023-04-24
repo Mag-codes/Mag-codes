730876347ebaca7b3b6e535456a8f80713ae86bf
unknown <Munezero2000>
2023-04-24 17:01:35 +0200

    Added foroward referencing

--- employee.sql
+++ employee.sql
@@ -28,6 +28,7 @@ INSERT INTO EMP_TABLE  VALUES('E006', 'Alfred', 'Butler', 345670, 'Chad');
 PACKAGE specification
 CREATE OR REPLACE PACKAGE EMP_DETAILS IS
 PROCEDURE set_record(p_emp_rec EMP_TABLE%ROWTYPE);
+PROCEDURE emp_count;
 FUNCTION get_record(p_emp_no EMP_TABLE.EMP_CODE%TYPE) RETURN EMP_TABLE%ROWTYPE;
 END EMP_DETAILS;
 /
@@ -37,13 +38,9 @@ CREATE OR REPLACE PACKAGE BODY EMP_DETAILS AS
 -- A procedure that take in an employee record and insert it into a database
 PROCEDURE set_record(p_emp_rec EMP_TABLE%ROWTYPE)
 is
--- Declaration variable to hold the number of Employees in the table
-Emp_num number;
 BEGIN 
     -- A statement to retrive the number of Employe in the table before insertion. 
-    SELECT count(*) into Emp_num from EMP_TABLE;
-    dbms_output.put_line('The number of Employees was '||Emp_num);
-
+    emp_count();
     -- A statement to Inset a new Employee
     INSERT INTO EMP_TABLE VALUES (p_emp_rec.EMP_CODE, 
                                 p_emp_rec.EMP_FIRST_NAME,
@@ -53,8 +50,7 @@ BEGIN
     -- Printing the detail of added Employee
     dbms_output.put_line('The Employee: '||p_emp_rec.EMP_FIRST_NAME||' '||p_emp_rec.EMP_LAST_NAME||' with '||p_emp_rec.EMP_CODE||' Code has been added');
     -- A statement to retrive the number of Employee in the table after Instertion.
-    SELECT count(*) into Emp_num from EMP_TABLE;
-    dbms_output.put_line('The new number of Employees is '||Emp_num);
+    emp_count();
 END set_record;
 
 -- A function to Retrive an employe  using His/Her Id
@@ -66,6 +62,15 @@ BEGIN
 SELECT * INTO Emp_detail FROM EMP_TABLE WHERE EMP_CODE = p_emp_no;
 RETURN Emp_detail;
 END get_record;
+
+-- A procedure to count Employee
+PROCEDURE emp_count is 
+Emp_num number;
+begin
+  SELECT count(*) into Emp_num from EMP_TABLE;
+    dbms_output.put_line('The number of Employees was '||Emp_num);
+end emp_count;
+
 end;
 
 -- Calling Program
@@ -74,7 +79,7 @@ Emp_detail EMP_TABLE%ROWTYPE;
 NEW_EMP EMP_TABLE%ROWTYPE;
 begin
 -- Inserting data into the NEw_EMP a rowtype attr 
-NEW_EMP.EMP_CODE := 'E020';
+NEW_EMP.EMP_CODE := 'E008';
 NEW_EMP.EMP_FIRST_NAME := 'Mbarushimana';
 NEW_EMP.EMP_LAST_NAME := 'Etienne';
 NEW_EMP.EMP_SALARY := 1000000;
